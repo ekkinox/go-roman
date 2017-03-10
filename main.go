9051f7ed5f1ee34e8b36c2c92ca6e3887d256a33
Jonathan VUILLEMIN <jonathan.vuillemin@doclerholding.com>
2017-03-10 14:43:21 +0100

    fix typo

--- main.go
+++ main.go
@@ -34,12 +34,10 @@ func Convert(num int) string {
 }
 
 func getRomanMap() [13]string {
-
     return [13]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 }
 
-func getDecimalMap() [13]int {
-
+func getDecimalMap() [13]int{
     return [13]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 }
 
